c99c6746313776cad11f2bd0748c5cc64625a641
unknown <cattuong.cloud@gmail.com>
2017-11-30 14:38:02 +0700

    broadcast emit

--- server/server.js
+++ server/server.js
@@ -14,15 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
   
-  
+  socket.emit('newMessage',{
+    from: 'admin',
+    text: 'welcome to app chat',
+    createdAt: new Date().getTime()
+  });
 
+  socket.broadcast.emit('newMessage',{
+    from: 'admin',
+    text: 'new user join',
+    createdAt: new Date().getTime()
+  });
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    // io.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    
   });
   socket.on('disconnect', () => {
     console.log('User was disconnected');
