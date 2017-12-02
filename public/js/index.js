603207a550bf2f1b7c3d55559be814ca79d806cd
unknown <cattuong.cloud@gmail.com>
2017-12-02 13:24:12 +0700

    create chat form with jquery

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-
 });
 
 socket.on('disconnect', function () {
@@ -12,7 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
-// socket.on('newEmail', function () {
-//   console.log('New email');
-// });
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
+});
